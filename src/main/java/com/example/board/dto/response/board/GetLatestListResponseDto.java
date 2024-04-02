831c64a7a44319cc04d5600e83af364d22cb7f92
devJiraynor <jiraynor@naver.com>
2024-04-02 15:31:38 +0900

    2024.04.02. 15:30

--- src/main/java/com/example/board/dto/response/board/GetLatestListResponseDto.java
+++ src/main/java/com/example/board/dto/response/board/GetLatestListResponseDto.java
@@ -2,18 +2,19 @@ package com.example.board.dto.response.board;
 
 import java.util.List;
 
+import com.example.board.dto.response.ResponseCode;
 import com.example.board.dto.response.ResponseDto;
+import com.example.board.dto.response.ResponseMessage;
 import com.example.board.dto.response.board.item.BoardListItem;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 public class GetLatestListResponseDto extends ResponseDto {
     private List<BoardListItem> latestList;
 
-    public GetLatestListResponseDto(String code, String message, List<BoardListItem> latestList) {
-        super(code, message);
+    public GetLatestListResponseDto(List<BoardListItem> latestList) {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
         this.latestList = latestList;
     }
 }

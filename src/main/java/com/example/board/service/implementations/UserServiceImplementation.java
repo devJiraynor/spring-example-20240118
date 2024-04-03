da7e0410336df69d2d07bb9adfaea0d392b2c3b3
devJiraynor <jiraynor@naver.com>
2024-04-03 15:51:06 +0900

    2024.04.03. 15:50

--- src/main/java/com/example/board/service/implementations/UserServiceImplementation.java
+++ src/main/java/com/example/board/service/implementations/UserServiceImplementation.java
@@ -26,17 +26,20 @@ public class UserServiceImplementation implements UserService {
             // findByEmail(email)
             // (email -> (조회 결과 인스턴스))
             UserEntity userEntity = userRepository.findByEmail(email);
+
+            // 2. 조회 결과에 따라 반환 결정
+            // 1) false이면 존재하지 않는 유저 응답처리 X
+            // 2) null 이면 존재하지 않는 유저 응답처리
+            if (userEntity == null) return ResponseDto.notExistUser();
+
+            // 3. 조회 결과 데이터를 성공 응답
+            return GetUserResponseDto.success(userEntity);
+
         } catch (Exception exception) {
             // 1-1. 조회 처리 중 데이터베이스 관련 예외가 발생하면 데이터베이스 에러 응답처리
             exception.printStackTrace();
             return ResponseDto.databaseError();
         }
-
-        // 2. 조회 결과에 따라 반환 결정
-        // 1) false이면 존재하지 않는 유저 응답처리 X
-        // 2) null 이면 존재하지 않는 유저 응답처리
-
-        // 3. 조회 결과 데이터를 성공 응답
 
     }
     
